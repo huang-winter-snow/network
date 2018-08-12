bf263149659a963360558507781ab5fb88d3b5c3
huangdongcheng <934313141@qq.com>
2018-08-12 22:04:38 +0800

    daytime cli

--- include/unp.h
+++ include/unp.h
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unitsd.h>
-#include <error.h>
+#include <unistd.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
+#define	SA	struct sockaddr
+
+#define	MAXLINE		4096	/* max text line length */
+#define	BUFFSIZE	8192	/* buffer size for reads and writes */
 
 void	 err_dump(const char *, ...);
 void	 err_msg(const char *, ...);
--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 		err_sys("socket error");
 	}
 
-	bzero(&servaddr, sizeof(servaddr));
+	memset(&servaddr, '\0', sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(13);
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
