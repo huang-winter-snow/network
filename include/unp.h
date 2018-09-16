9ae6bf29c27e3fc2d7934afd0cd0c8f5d912bc3c
huangdongcheng <934313141@qq.com>
2018-09-16 22:03:24 +0800

    tcp cli server

--- include/unp.h
+++ include/unp.h
@@ -12,9 +12,10 @@
 
 #define	MAXLINE		4096	/* max text line length */
 #define	BUFFSIZE	8192	/* buffer size for reads and writes */
+#define SERV_PORT	9877
 
-void	 err_dump(const char *, ...);
-void	 err_msg(const char *, ...);
-void	 err_quit(const char *, ...);
-void	 err_ret(const char *, ...);
-void	 err_sys(const char *, ...);
+void    err_dump(const char *, ...);
+void	err_msg(const char *, ...);
+void	err_quit(const char *, ...);
+void	err_ret(const char *, ...);
+void	err_sys(const char *, ...);
---
+++ tcpcliserver/tcpcli01.c
@@ -0,0 +1,19 @@
+#include "unp.h"
+int main(int argc, char **argv)
+{
+	int sockfd;
+	struct sockaddr_in servaddr;
+	if (argc != 2) {
+		err_quit("usage: tcpcli <IPaddress>");
+	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	inet_pton(AF_INET, agrv[1], &servaddr.sin_addr);
+
+	connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+	str_cli(stdin, sockfd);
+	exit(0);
+}
